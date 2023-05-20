f1c57e8a9d2057dcdd5ea46157bd5f482c22d606
aalexr <31433793+aalexr@users.noreply.github.com>
2023-05-20 17:31:52 +0200

    setting thru args

--- pdf_sections.py
+++ pdf_sections.py
@@ -1,30 +1,48 @@
 from PyPDF2 import PdfReader, PdfWriter
+import sys, getopt
 
 if __name__ == '__main__':
-    pdfPath = 'insert your path here'
-    reader = PdfReader(pdfPath)
-    writer = PdfWriter()
 
-    # const
     pages_per_sheet = 4
     sheets_per_section = 4
+    pdfPath = ""
+    outputPath = "./out.pdf"
+
+    for option, arg in getopt.getopt(sys.argv[1:], "hs:i:o:")[0]:
+        if option == '-h':
+            print("-s Sheets per section. Default is 4. -i input file -o output file")
+        elif option == '-s':
+            sheets_per_section = int(arg)
+        elif option == "-i":
+            pdfPath = str(arg)
+        elif option == "-o":
+            outputPath = str(arg)
+
+    if (pdfPath == ""):
+        raise Exception("No input file provided")
+    
     pages_per_section = sheets_per_section * pages_per_sheet
+
+    reader = PdfReader(pdfPath)
 
     length = len(reader.pages)
     num_sections = length // pages_per_section
-    for i in range(num_sections):
-        section_from = pages_per_section * i
-        section_to = pages_per_section * (i + 1) - 1
 
-        for j in range(pages_per_section // 2):
-            first_page = section_from + j
-            last_page = section_to - j
-            if j % 2 == 0:
-                [first_page, last_page] = [last_page, first_page]
+    writer = PdfWriter()
+
+    for section in range(num_sections):
+        section_from = pages_per_section * section
+        section_to = pages_per_section * (section + 1) - 1
+
+        for page_offset in range(pages_per_section // 2):
+            first_page = section_from + page_offset
+            last_page = section_to - page_offset
+            if page_offset % 2 == 0:
+                first_page, last_page = last_page, first_page
 
             writer.add_page(reader.pages[first_page])
             writer.add_page(reader.pages[last_page])
 
-        writer.write('output path here')
+        writer.write(outputPath)
 
     writer.close()
